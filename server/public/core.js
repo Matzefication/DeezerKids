19cdb65f0c948e464863069e5d53e65c0f8231a7
Matzefication <Mathias@Kaniut.de>
2017-05-23 09:51:26 +0200

    Update core.js

--- server/public/core.js
+++ server/public/core.js
@@ -38,42 +38,42 @@ DeezerKids.config(function($routeProvider) {
 
 angapp.controller("AppController", function($scope, $route, $routeParams, $location, $rootScope) {
     
-    $scope.formData = {};
+	$scope.formData = {};
 
-    // when landing on the page, get all accounts and show them
-    $http.get('/api/accounts')
-        .success(function(data) {
-            $scope.accounts = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+	// when landing on the page, get all accounts and show them
+	$http.get('/api/accounts')
+		.success(function(data) {
+			$scope.accounts = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
 
-    // when submitting the add form, send the text to the node API
-    $scope.createAccount = function() {
-        $http.post('/api/accounts', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.accounts = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+	// when submitting the add form, send the text to the node API
+	$scope.createAccount = function() {
+		$http.post('/api/accounts', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {}; // clear the form so our user is ready to enter another
+				$scope.accounts = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
 
-    // delete a account after checking it
-    $scope.deleteAccount = function(id) {
-        $http.delete('/api/accounts/' + id)
-            .success(function(data) {
-                $scope.accounts = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };    
+	// delete a account after checking it
+	$scope.deleteAccount = function(id) {
+		$http.delete('/api/accounts/' + id)
+			.success(function(data) {
+				$scope.accounts = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};    
     
 	// Init config
 	$rootScope.title = 'DZ';
