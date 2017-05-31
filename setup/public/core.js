04da5064b9e45a6a6a38a2e577a3a9dd0dfe2d95
Matzefication <Mathias@Kaniut.de>
2017-05-31 17:16:54 +0200

    Update core.js

--- setup/public/core.js
+++ setup/public/core.js
@@ -1,35 +1,35 @@
 var LOGNS = 'DeezerKids:';
 var APP_ID = '236482';
-var CHANNEL_URL = 'http://www.beup2date.com/DeezerKids/channel.html';
 
 var DeezerKids = angular.module('DeezerKids', ["firebase"]);
 
 DeezerKids.controller('AppController', function($scope, $rootScope, $http, $firebaseObject) {
 	
-	//////////////////////////////////////////////////////
-	// TEST TEST TEST TEST TEST
-	//////////////////////////////////////////////////////
-	$http.post('http://beup2date.com/DeezerKids/devices').then(
-		function(result) {
-			console.log(LOGNS, result);
-		},
-		function(error) {
-			console.log(LOGNS, 'Error: ' + error);
-		});	
+	$scope.completed = false;	// Setup not yet completed
 	
 	//////////////////////////////////////////////////////
-	// STEP1: check if device setup already completed
+	// STEP 1: check WLAN settings
 	//////////////////////////////////////////////////////
-	$scope.completed = false;
 	$scope.step = 1;
+	
+	//////////////////////////////////////////////////////
+	// STEP 2: check connectivity and connect to firebase
+	//////////////////////////////////////////////////////	
+	$scope.step = 2;
+	var ref = firebase.database().ref();
+
+	//////////////////////////////////////////////////////
+	// STEP 3: check device id
+	//////////////////////////////////////////////////////	
+	$scope.step = 3;
 	$http.get('/api/account').then(
 		function(result) {
 			if (result.data == null) {
-				console.log(LOGNS, 'Starting DeezerKids setup');
+				console.log(LOGNS, 'Creating Device-ID');
 				$scope.completed = false;
 				$scope.step = 2;
 			} else {
-				console.log(LOGNS, 'DeezerKids setup already completed');
+				console.log(LOGNS, 'Device-ID already set');
 				$scope.account = result;
 				$scope.completed = true;
 			}
@@ -38,37 +38,36 @@ DeezerKids.controller('AppController', function($scope, $rootScope, $http, $fire
 		function(error) {
 			console.log(LOGNS, 'Error: ' + error);
 		});	
-
-	//////////////////////////////////////////////////////
-	// STEP2: setup WLAN connection
-	//////////////////////////////////////////////////////
-	$scope.setupWLAN = function() {
-	};
 	
 	//////////////////////////////////////////////////////
-	// STEP3: connect to Deezer account
+	// STEP4: connect to Deezer account
 	//////////////////////////////////////////////////////
 	$scope.connectAccount = function() {
-		$http.get('http://beup2date.com/DeezerKids/devices/123').then(
-			function(result) {
-				console.log(LOGNS, result);
-			},
-			function(error) {
-				console.log(LOGNS, 'Error: ' + error);
-			});	
+		DZ.init({
+			appId: APP_ID,
+			channelUrl: 'http://www.beup2date.com/DeezerKids/devices/' + $scope.deviceid + '/setAccessToken'
+		});
+		console.log(LOGNS, 'Deezer-API initialiazed successfully');
+		
+		DZ.login(function(response) {
+			if (response.authResponse) {
+				console.log(LOGNS, 'successfully logged in with Token ', response.authResponse.accessToken);
+				$scope.account.accessToken = response.authResponse.accessToken;
+				$scope.account.expire = response.authResponse.expire;
+				$scope.account.userID = response.userID;				
+				createAccount();
+			} else {
+				console.log(LOGNS, 'login aborded by user');
+				$scope.login = false;
+			}
+		}, {scope: 'basic_access,email,offline_access,manage_library'});		
 	};
 	
 	//////////////////////////////////////////////////////
 	// STEP4: select playlist from Deezer account
 	//////////////////////////////////////////////////////
 	$scope.selectPlaylist = function() {
-		DZ.init({
-			appId: APP_ID,
-			channelUrl: CHANNEL_URL
-		});	
-		console.log(LOGNS, 'Deezer-API initialiazed successfully');
 
-		console.log(LOGNS, 'login clicked');	
 	};
 
 	//////////////////////////////////////////////////////
@@ -99,25 +98,5 @@ DeezerKids.controller('AppController', function($scope, $rootScope, $http, $fire
 			function(error) {
 				console.log(LOGNS, 'Error while deleting account: ' + error);
 			});
-	};		
-
-
-	// --------------------------------------------------- Methods
-
-	$scope.doLogin = function() {
-		console.log(LOGNS, 'login clicked');
-
-		DZ.login(function(response) {
-			if (response.authResponse) {
-				console.log(LOGNS, 'successfully logged in with Token ', response.authResponse.accessToken);
-				$scope.account.accessToken = response.authResponse.accessToken;
-				$scope.account.expire = response.authResponse.expire;
-				$scope.account.userID = response.userID;				
-				createAccount();
-			} else {
-				console.log(LOGNS, 'login aborded by user');
-				$scope.login = false;
-			}
-		}, {scope: 'basic_access,email,offline_access,manage_library'});
 	};
 });
