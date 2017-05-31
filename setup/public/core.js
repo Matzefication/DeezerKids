320fa05bac57b00ecc738e42c5b24c90523557fa
Matzefication <Mathias@Kaniut.de>
2017-05-31 16:38:11 +0200

    Create core.js

---
+++ setup/public/core.js
@@ -0,0 +1,123 @@
+var LOGNS = 'DeezerKids:';
+var APP_ID = '236482';
+var CHANNEL_URL = 'http://www.beup2date.com/DeezerKids/channel.html';
+
+var DeezerKids = angular.module('DeezerKids', []);
+
+DeezerKids.controller('AppController', function($scope, $rootScope, $http) {
+	
+	//////////////////////////////////////////////////////
+	// TEST TEST TEST TEST TEST
+	//////////////////////////////////////////////////////
+	$http.post('http://beup2date.com/DeezerKids/devices').then(
+		function(result) {
+			console.log(LOGNS, result);
+		},
+		function(error) {
+			console.log(LOGNS, 'Error: ' + error);
+		});	
+	
+	//////////////////////////////////////////////////////
+	// STEP1: check if device setup already completed
+	//////////////////////////////////////////////////////
+	$scope.completed = false;
+	$scope.step = 1;
+	$http.get('/api/account').then(
+		function(result) {
+			if (result.data == null) {
+				console.log(LOGNS, 'Starting DeezerKids setup');
+				$scope.completed = false;
+				$scope.step = 2;
+			} else {
+				console.log(LOGNS, 'DeezerKids setup already completed');
+				$scope.account = result;
+				$scope.completed = true;
+			}
+			console.log(LOGNS, result);
+		},
+		function(error) {
+			console.log(LOGNS, 'Error: ' + error);
+		});	
+
+	//////////////////////////////////////////////////////
+	// STEP2: setup WLAN connection
+	//////////////////////////////////////////////////////
+	$scope.setupWLAN = function() {
+	};
+	
+	//////////////////////////////////////////////////////
+	// STEP3: connect to Deezer account
+	//////////////////////////////////////////////////////
+	$scope.connectAccount = function() {
+		$http.get('http://beup2date.com/DeezerKids/devices/123').then(
+			function(result) {
+				console.log(LOGNS, result);
+			},
+			function(error) {
+				console.log(LOGNS, 'Error: ' + error);
+			});	
+	};
+	
+	//////////////////////////////////////////////////////
+	// STEP4: select playlist from Deezer account
+	//////////////////////////////////////////////////////
+	$scope.selectPlaylist = function() {
+		DZ.init({
+			appId: APP_ID,
+			channelUrl: CHANNEL_URL
+		});	
+		console.log(LOGNS, 'Deezer-API initialiazed successfully');
+
+		console.log(LOGNS, 'login clicked');	
+	};
+
+	//////////////////////////////////////////////////////
+	// STEP5: save account data to mongodb
+	//////////////////////////////////////////////////////
+	$scope.saveAccount = function() {
+		$http.post('/api/accounts', $scope.account).then(
+			function(result) {
+				console.log(LOGNS, 'Account saved to database', result);
+				$scope.completed = true;
+			},
+			function(error) {
+				console.log(LOGNS, 'Error while saving account: ' + error);
+				$scope.completed = false;
+			});
+	};
+	
+	//////////////////////////////////////////////////////
+	// COMPLETED: setup already completed
+	//////////////////////////////////////////////////////
+	$scope.deleteAccount = function deleteAccount() {
+		$http.delete('/api/account').then(
+			function(result) {
+				$scope.account = { };
+				console.log(LOGNS, 'Account successfully deleted');
+				$scope.login = false;
+			},
+			function(error) {
+				console.log(LOGNS, 'Error while deleting account: ' + error);
+			});
+	};		
+
+
+	// --------------------------------------------------- Methods
+
+	$scope.doLogin = function() {
+		console.log(LOGNS, 'login clicked');
+
+		DZ.login(function(response) {
+			if (response.authResponse) {
+				console.log(LOGNS, 'successfully logged in with Token ', response.authResponse.accessToken);
+				$scope.account.accessToken = response.authResponse.accessToken;
+				$scope.account.expire = response.authResponse.expire;
+				$scope.account.userID = response.userID;				
+				createAccount();
+			} else {
+				console.log(LOGNS, 'login aborded by user');
+				$scope.login = false;
+			}
+		}, {scope: 'basic_access,email,offline_access,manage_library'});
+	};
+});
