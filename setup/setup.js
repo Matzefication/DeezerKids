54c5f6d776eae0d5d99da425b20cd7e0dfc68cc2
Matzefication <Mathias@Kaniut.de>
2017-06-01 13:55:10 +0200

    Update setup.js

--- setup/setup.js
+++ setup/setup.js
@@ -1,33 +1,58 @@
-    // set up ========================
-    var LOGNS = 'DeezerKids:';
-    var express = require('express');
-    var app = express();                              // create our app w/ express
-    var mongoose = require('mongoose');               // mongoose for mongodb
-    var morgan = require('morgan');                   // log requests to the console (express4)
-    var bodyParser = require('body-parser');          // pull information from HTML POST (express4)
-    var methodOverride = require('method-override');  // simulate DELETE and PUT (express4)
+var async               = require("async"),
+    wifi_manager        = require("./app/wifi_manager")(),
+    dependency_manager  = require("./app/dependency_manager")(),
+    
+    express             = require('express'),
+    app                 = express(),
+    mongoose            = require('mongoose'),
+    morgan              = require('morgan'),
+    bodyParser          = require('body-parser'),
+    methodOverride      = require('method-override'),
+    
+    config              = require("./config.json"),   
+    LOGNS               = 'DeezerKids:';
 
-    // configuration =================
-    console.log(LOGNS, "connecting to local database");
-    mongoose.connect('mongodb://localhost/DeezerKids');     // connect to mongoDB database
+async.series([
+    /////////////////////////////////////////////////////////////////////
+    // STEP 1: Check if we have the required dependencies installed
+    /////////////////////////////////////////////////////////////////////
+    function test_deps(next_step) {
+        console.log(LOGNS, "checking required dependencies installed");
+        dependency_manager.check_deps({
+            "binaries": ["dhcpd", "hostapd", "iw"],
+            "files":    ["/etc/init.d/isc-dhcp-server"]
+        }, function(error) {
+            if (error) console.log(" * Dependency error, did you run `sudo npm run-script provision`?");
+            next_step(error);
+        });
+    },
+    
+    /////////////////////////////////////////////////////////////////////
+    // STEP 2: Connect to internal Mongo-DB
+    /////////////////////////////////////////////////////////////////////
+    function connect_db(next_step) {
+        console.log(LOGNS, "connecting to local database");
+        mongoose.connect('mongodb://localhost/DeezerKids');
+        var db = mongoose.connection;
 
-    var db = mongoose.connection;
-
-    db.on('error', function() {
-        console.error.bind(console, 'connection error:');
-    });
-
-    db.once('open', function() {
-        console.log(LOGNS, "succesful connected to database");
-        // we're connected!
-        // define model =================
-        var DeezerAccount = mongoose.model('DeezerAccount', {
-            active: Boolean,
-            accessToken: String,
-            expire: String,
-            userID: String
+        db.on('error', function() {
+            console.error.bind(console, LOGNS + 'connection error:');
         });
 
+        db.once('open', function() {
+            console.log(LOGNS, "succesfully connected to database");
+
+            var device = mongoose.model('Device', {
+                ID: String
+            });
+        });    
+    },
+
+    /////////////////////////////////////////////////////////////////////
+    // STEP 3: Host HTTP-Server for User-Inerfaces
+    /////////////////////////////////////////////////////////////////////
+    function start_http_server(next_step) {
+        console.log(LOGNS, "starting webserver for user-interfaces");
         app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
         app.use(morgan('dev'));                                         // log every request to the console
         app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -36,57 +61,65 @@
         app.use(methodOverride());
 
         // routes ---------------------------------------------------------------------
-        // get all stored accounts
-        app.get('/api/account', function(req, res) {
+        // get the stored device
+        app.get('/api/device', function(req, res) {
             // use mongoose to get all accounts in the database
-            DeezerAccount.findOne(function(err, account) {
+            device.findOne(function(err, result) {
                 // if there is an error retrieving, send the error. nothing after res.send(err) will execute
                 if (err)
                     res.send(err)
-                res.json(account); // return all accounts in JSON format
+                res.json(result); // return the device in JSON format
             });
         });
 
-        // create account and send back all accounts after creation
-        app.post('/api/account', function(req, res) {
+        // create device and send back after creation
+        app.post('/api/device', function(req, res) {
             // delete existing entries (there should only be one at same time)
-            DeezerAccount.deleteMany();
+            device.deleteMany();
             
-            // create an account, information comes from AJAX request from Angular
-            DeezerAccount.create({
-                active : true,
-                accessToken : req.accessToken,
-                expire : req.expire,
-                userID : req.userID
-            }, function(err, account) {
+            // create device, information comes from AJAX request from Angular
+            device.create({
+                ID : req.ID
+            }, function(err, result) {
                 if (err)
-                    res.send(err);
-
-                // get and return all the accounts after you create another
-                DeezerAccount.findOne(function(err, account) {
-                    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-                    if (err)
-                        res.send(err)
-                    res.json(account); // return all accounts in JSON format
-                });
+                    res.send(err)
+                res.json(result); // return all accounts in JSON format
             });
-
-        });
-
-        // delete a account
-        app.delete('/api/account', function(req, res) {
-            DeezerAccount.remove(function(err, account) {
-                if (err)
-                    res.send(err);
-            });
-        });
-
+        });        
+        
+        // reset the device
+        app.delete('/api/device', function(req, res) {
+            // delete existing entries (there should only be one at same time)
+            device.deleteMany();
+        });        
+        
         // application -------------------------------------------------------------
         app.get('*', function(req, res) {
             res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
         });
 
         // listen (start app with node server.js) ======================================
-        app.listen(8000);
-        console.log(LOGNS, "listening on port 8000");
-    });
+        app.listen(config.server.port);
+        console.log(LOGNS, "listening on port " + config.server.port);
+    },
+    
+    /////////////////////////////////////////////////////////////////////
+    // STEP 4: Check if wifi is enabled / connected
+    /////////////////////////////////////////////////////////////////////
+    function test_is_wifi_enabled(next_step) {
+        wifi_manager.is_wifi_enabled(function(error, result_ip) {
+            if (result_ip) {
+                console.log(LOGNS, "\nWifi is enabled, and IP " + result_ip + " assigned");
+                // process.exit(0);
+            } else {
+                console.log(LOGNS, "\nWifi is not enabled, Enabling AP for self-configure");
+            }
+            next_step(error);
+        });
+    },    
+    
+], function(error, results) {
+    if (error) {
+        console.log("ERROR: " + error);
+    }
+});
