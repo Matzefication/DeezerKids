41cd63afb2983cfa1564c0ac4519ed51f74b0b9c
Matzefication <Mathias@Kaniut.de>
2017-06-01 14:33:08 +0200

    Update setup.js

--- setup/setup.js
+++ setup/setup.js
@@ -19,12 +19,12 @@ async.series([
     // STEP 1: Check if we have the required dependencies installed
     /////////////////////////////////////////////////////////////////////
     function test_deps(next_step) {
-        console.log(LOGNS, "checking required dependencies installed");
+        logger.info("checking required dependencies installed");
         dependency_manager.check_deps({
             "binaries": ["dhcpd", "hostapd", "iw"],
             "files":    ["/etc/init.d/isc-dhcp-server"]
         }, function(error) {
-            if (error) console.log(" * Dependency error, did you run `sudo npm run-script provision`?");
+            if (error) logger.error("dependency error, did you run `sudo npm run-script provision`?");
             next_step(error);
         });
     },
@@ -33,20 +33,22 @@ async.series([
     // STEP 2: Connect to internal Mongo-DB
     /////////////////////////////////////////////////////////////////////
     function connect_db(next_step) {
-        console.log(LOGNS, "connecting to local database");
+        logger.info("connecting to local database");
         mongoose.connect('mongodb://localhost/DeezerKids');
         var db = mongoose.connection;
 
-        //db.on('error', function() {
-        //    console.error.bind(console, LOGNS + 'connection error:');
-        //});
+        db.on('error', function() {
+            console.error.bind(console, LOGNS + 'connection error:');
+        });
 
         db.once('open', function() {
-            console.log(LOGNS, "succesfully connected to database");
+            logger.success("succesfully connected to database");
 
             var device = mongoose.model('Device', {
                 ID: String
             });
+            
+            next_step(null);
         });    
     },
 
@@ -54,7 +56,7 @@ async.series([
     // STEP 3: Host HTTP-Server for User-Inerfaces
     /////////////////////////////////////////////////////////////////////
     function start_http_server(next_step) {
-        console.log(LOGNS, "starting webserver for user-interfaces");
+        logger.info("starting webserver for user-interfaces");
         app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
         app.use(morgan('dev'));                                         // log every request to the console
         app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -102,7 +104,9 @@ async.series([
 
         // listen (start app with node server.js) ======================================
         app.listen(config.server.port);
-        console.log(LOGNS, "listening on port " + config.server.port);
+        logger.success("listening on port " + config.server.port);
+        
+        next_step(null);
     },
     
     /////////////////////////////////////////////////////////////////////
@@ -111,10 +115,10 @@ async.series([
     function test_is_wifi_enabled(next_step) {
         wifi_manager.is_wifi_enabled(function(error, result_ip) {
             if (result_ip) {
-                console.log(LOGNS, "\nWifi is enabled, and IP " + result_ip + " assigned");
+                logger.info("\nWifi is enabled, and IP " + result_ip + " assigned");
                 // process.exit(0);
             } else {
-                console.log(LOGNS, "\nWifi is not enabled, Enabling AP for self-configure");
+                logger.info("\nWifi is not enabled, Enabling AP for self-configure");
             }
             next_step(error);
         });
@@ -122,6 +126,6 @@ async.series([
     
 ], function(error, results) {
     if (error) {
-        console.log("ERROR: " + error);
+        logger.error(error);
     }
 });
