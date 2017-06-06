539716fbd39488b1b31eabc9ed20dec46028c25c
Matzefication <Mathias@Kaniut.de>
2017-06-06 13:15:45 +0200

    Update config.route.js

--- app/modules/startup/config.route.js
+++ app/modules/startup/config.route.js
@@ -1,1 +1,24 @@
+(function () {
+    'use strict';
 
+    angular
+        .module('DeezerKids')
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$routeProvider'];
+
+    function config($routeProvider) {
+        $routeProvider
+            .when('/startup', {
+                templateUrl: 'app/modules/startup/startup.view.html',
+                controller: 'StartupController',
+                controllerAs: 'vm'
+            })
+    }
+
+    run.$inject = [];
+
+    function run() {}
+
+})();
