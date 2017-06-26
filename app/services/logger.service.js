67972b327802330c524530dd1dbdb223793f7b96
Matzefication <Mathias@Kaniut.de>
2017-06-26 15:29:32 +0200

    Update logger.service.js

--- app/services/logger.service.js
+++ app/services/logger.service.js
@@ -1,29 +1,26 @@
 var config  = require("../config.json");
 
-/*****************************************************************************\
-    Return a set of functions which we can use to log to console
-\*****************************************************************************/
-module.exports = function() {
-    _error = function(message) {
-      console.log(config.logger.message + "ERROR:\t" + message);
-    };
-    
-    _warn = function(message) {
-      console.log(config.logger.message + "WARNING:\t" + message);
-    };
+(function () {
+    'use strict';
 
-    _info = function(message) {
-      console.log(config.logger.message + "INFO:\t" + message);
-    };
+    angular
+        .module('DeezerKids')
+        .factory('LoggerService', LoggerService);
 
-    _success = function(message) {
-      console.log(config.logger.message + "SUCCESS:\t" + message);
-    };
+    LoggerService.$inject = ['$rootScope'];
 
-    return {
-        error:    _error,
-        warn:     _warn,
-        info:     _info,
-        success:  _success,
-    };
-}
+    function LoggerService($rootScope) {
+        var service = {};
+        
+        service.Error = function (message) {
+            logMessage('Error', message);
+        }
+        
+        return service;
+        
+        function logMessage(type, message) {
+            console.log(config.logger.message + type + ":\t" + message);
+        }        
+    }
+
+})();
